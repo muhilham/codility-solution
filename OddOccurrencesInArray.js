9076f8d75ffd9742ee6b49b60eeaf3529cf1bc38
Muhammad Ilham <muhilham@go-jek.com>
2016-08-22 15:26:15 +0700

    using bitwise xor operator

--- OddOccurrencesInArray.js
+++ OddOccurrencesInArray.js
@@ -1,46 +1,34 @@
 /**
- * https://codility.com/demo/results/trainingW6PGDQ-5ZC/
+ * https://codility.com/demo/results/trainingGAB8DR-YGN/
  */
 'use strict';
-
-function solution(N) {
-  if (!Array.isArray(N)) {
-    return 0;
-  }
-  if (N.length == 1) {
-    return N[0];
-  }
-  if (N.length < 1 || N.length % 2 == 0) {
-    return 0;
-  }
-
-  var value = N;
-  value.sort(function (a, b) {
-    return a - b;
-  });
-
+function solution(A) {
   var answer = 0;
-
-  value.forEach(function (element, index) {
-    if (index !== value.length - 1) {
-      if (element !== value[index-1] && element !== value[index+1]) {
-        answer = element;
-      }
-    }
-
-    if (index == value.length - 1 && element !== value[index-1]) {
-      answer = element;
-    }
+  A.forEach(function (element) {
+    answer = answer ^ element;
   });
-
   return answer;
 }
 
-let input = [9, 3, 9, 3 ,9, 7, 9];
-
+let input = [9, 3, 9, 3, 5];
 console.log(solution(input));
-console.log(solution([1]));
-console.log(solution([]));
-console.log(solution([201]));
-console.log(solution([1, 2]));
-console.log(solution([1, 2, 1, 1]));
+
+/**
+ * turn all of the decimal into binary
+ * do XOR operation in all of the binary
+ * since XOR operator will make result of your binary into zero when it find the same number
+ * example input = [9, 3, 9, 9, 5]
+ * what will happen are :
+   1001 (9) ==> a
+   0011 (3) ==> b
+   1001 (9) ==> c
+   0011 (3) ==> d
+   0101 (5) ==> e
+   ======== XOR
+   0101 (5)
+
+   a xor c = 0
+   b xor d = 0
+ *
+ * note : xor means campared element has to resulting odd value in order to get 1.
+ */
