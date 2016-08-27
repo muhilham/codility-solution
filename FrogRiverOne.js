87766035b1ee54a8628cd809a731f40419305374
Muhammad Ilham <muhilham@go-jek.com>
2016-08-27 22:34:15 +0700

    still 60% performance :(

--- FrogRiverOne.js
+++ FrogRiverOne.js
@@ -1,40 +1,26 @@
 /**
  * https://codility.com/demo/take-sample-test/frog_river_one/
- * https://codility.com/demo/results/trainingQFM27H-PXH/
+ * https://codility.com/demo/results/training4Y8DR2-G9B/
  */
 
 
 'use strict';
 
-
 function solution(x, A) {
-  var position = A.indexOf(x);
-
-  if (position === -1) {
-    return position;
-  }
-
-  var indexes = [];
   var tempInc = 0;
-  for (var i = 1; i <= x; i++) {
-    if (A.indexOf(i) === -1) {
-      tempInc = -1;
-    }
-    indexes.push(A.indexOf(i));
-  }
-
-  if (tempInc === -1) {
-    return -1;
-  }
-
+  var current = 0;
   var temp_ = -1;
-  indexes.forEach(function (element) {
-    if (element > temp_) {
-      temp_ = element;
+  for (var i = 1; i <= x; i++) {
+    current = A.indexOf(i);
+    if (current === -1) {
+      tempInc = -1;
+      break;
     }
-  });
-
-  return temp_;
+    if (current > temp_) {
+      temp_ = current;
+    }
+  }
+  return (tempInc === -1) ? tempInc : temp_;
 }
 
 var input = [1, 3, 1, 4, 2, 3, 5, 4];
@@ -43,7 +29,7 @@ var input2 = [2, 2, 2, 2, 2, 2];
 console.log(solution(3, input));
 console.log(solution(2, input2));
 // console.log(solution(1, [1]));
-// console.log(solution(3, [1, 2, 3, 5, 3, 1]));
+console.log(solution(9, [1, 2, 3, 5, 3, 1]));
 console.log(solution(3, [1, 3, 1, 3, 2, 1, 3]));
 
 // in which index that frog could escape ?
