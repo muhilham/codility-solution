a5a95dc82aaadb987247d828c79a88eb68b44177
Muhammad Ilham <muhilham@go-jek.com>
2016-08-28 00:29:42 +0700

    finally got it. Counting Elements =>  FrogRiverOne

--- FrogRiverOne.js
+++ FrogRiverOne.js
@@ -1,26 +1,36 @@
 /**
  * https://codility.com/demo/take-sample-test/frog_river_one/
- * https://codility.com/demo/results/training4Y8DR2-G9B/
+ * https://codility.com/demo/results/trainingPTCWPM-Q4U/
  */
 
 
 'use strict';
 
 function solution(x, A) {
-  var tempInc = 0;
-  var current = 0;
-  var temp_ = -1;
-  for (var i = 1; i <= x; i++) {
-    current = A.indexOf(i);
-    if (current === -1) {
-      tempInc = -1;
-      break;
+  if (A.length === 1) {
+    if (A[0] !== 1 || x !== 1) {
+      return -1;
     }
-    if (current > temp_) {
-      temp_ = current;
-    }
+    return 0;
   }
-  return (tempInc === -1) ? tempInc : temp_;
+
+  var sum = (x*(x+1)) / 2;
+
+  var isRepeated = [];
+  var beforeSum = 0;
+  var answer = 0;
+  A.forEach(function (element, index) {
+    if (!isRepeated[element] && element <= x) {
+      beforeSum += element;
+      isRepeated[element] = true;
+      if (beforeSum === sum) {
+        return answer = index;
+      }
+    }
+    return;
+  });
+
+  return (answer === 0) ? -1 : answer;
 }
 
 var input = [1, 3, 1, 4, 2, 3, 5, 4];
@@ -28,7 +38,7 @@ var input2 = [2, 2, 2, 2, 2, 2];
 
 console.log(solution(3, input));
 console.log(solution(2, input2));
-// console.log(solution(1, [1]));
+console.log(solution(1, [1]));
 console.log(solution(9, [1, 2, 3, 5, 3, 1]));
 console.log(solution(3, [1, 3, 1, 3, 2, 1, 3]));
 
