cc63677d84c91f13ac59ab65242b28c8992fc0a8
Muhammad Ilham <muhilham@go-jek.com>
2016-08-27 18:07:43 +0700

    when you think to complex, turns out it's that simple

--- PermCheck.js
+++ PermCheck.js
@@ -1,6 +1,7 @@
 /**
  * https://codility.com/programmers/task/perm_check/
  * https://codility.com/demo/take-sample-test/perm_check/
+ * https://codility.com/demo/results/trainingBU5S9V-ANU/
  */
 
 'use strict';
@@ -18,40 +19,33 @@ function solution(input) {
   if (inputSort[0] !== 1) {
     return 0;
   }
-  if ((inputSort.length === 2)) {
-    if (inputSort[1] - inputSort[0] !== 1) {
-      return 0;
-    }
-  }
-  var answer = 0;
+
+  var answer = 1;
   var tempsub, temp2sub, tempMod, tempMod2;
   inputSort.forEach(function (element, index) {
     if (index !== 0) {
-      temp2sub = tempsub;
-      tempsub = element - inputSort[index-1];
-
-      tempMod2 = tempMod;
-      tempMod = element % inputSort[index-1];
-
-      if (index > 1) {
-        if(tempsub === temp2sub || (tempMod === tempMod2) ) {
-          answer = 1;
-        }
+      if (element - inputSort[index-1] !== 1) {
+        return answer = 0;
       }
     }
   });
-
-  answer = (inputSort.length > 2) ? answer : 1;
   return answer;
 };
 
 
 var input = [4, 1, 3, 2];
 
-console.log(solution(input));
+// solution(input);
+// console.log(solution(input));
+// console.log(solution([]));
+// console.log(solution([0]));
+// console.log(solution([1]));
+// console.log(solution([2]));
+console.log(solution([1,2]));
+console.log(solution([2, 3]));
 console.log(solution([2, 4, 8, 16]));
 console.log(solution([1, 3, 4]));
-console.log(solution([1, 3, 5, 7]));
-console.log(solution([2, 5, 11, 23]));
-console.log(solution([2, 5]));
-console.log(solution([2]));
+// console.log(solution([1, 3, 5, 7]));
+// console.log(solution([2, 5, 11, 23]));
+// console.log(solution([2, 5]));
+// console.log(solution([2]));
